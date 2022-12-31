86b473447b98c64d9c18846386b6d7906c1fabc0
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-01 02:13:02 +0530

    Delete travel-Diaries directory

--- travel-Diaries/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use("/",(req,res,next)=>{
-  return res.send("Hi ");
-})
-
-app.listen(5000, () => console.log("Listening to localhost Port 5000"));
