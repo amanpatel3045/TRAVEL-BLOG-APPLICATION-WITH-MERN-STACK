9df69da09fe95359fa9e69d147fb905ed4f260e5
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-01 18:34:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./header/Header";
+import Home from "./home/Home";
+import Diaries from "./diaries/Diaries";
+import Auth from "./auth/Auth";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+
+      <section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/diaries" element={<Diaries />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Box} from "@mui/system";
+const Home = () => {
+  return <Box width="100%" height="90vh">
+<img src="" alt=""/>
+  </Box>;
+};
+
+export default Home;
